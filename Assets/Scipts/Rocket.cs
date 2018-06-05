4288b2d8564f8f9cbfbb311f3795047fbdbff00e
Paul Rolland <paulrollanddev@gmail.com>
2018-06-05 18:47:17 +0100

    05/06/2018 Added  Audio

---
+++ Assets/Scipts/Rocket.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+    bool mySound;
+    bool myToggle;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        ProcessInput();
+     
+        //sound 
+        if (mySound == true && myToggle == false)
+        {
+            audioSource.Play();
+            myToggle = true;
+        }
+        if (mySound == false && myToggle == true)
+        {
+            audioSource.Stop();
+            myToggle = false;
+        }
+
+    }
+
+    private void ProcessInput()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            mySound = true;
+        } else
+        {
+            mySound = false;
+        }
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        } 
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+}
