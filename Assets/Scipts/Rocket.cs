b59d5c74f88453b45d934b272828e7d07b275856
Paul Rolland <paulrollanddev@gmail.com>
2018-06-26 22:48:43 +0100

    26/06/2018 Build number 2

--- Assets/Scipts/Rocket.cs
+++ Assets/Scipts/Rocket.cs
@@ -1,60 +1,117 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Rocket : MonoBehaviour {
+public class Rocket : MonoBehaviour
+{
+    enum State { Alive, Dying, Transcending }
+    State state = State.Alive;
+
+    [SerializeField] float rcsTrust = 100f;
+    [SerializeField] float mainTrust = 100f;
+    [SerializeField] float levelTimeDelay = 2f;
+
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip death;
+    [SerializeField] AudioClip nextLevel;
+
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem deathParticles;
+    [SerializeField] ParticleSystem nextLevelParticles;
 
     Rigidbody rigidBody;
     AudioSource audioSource;
-    bool mySound;
-    bool myToggle;
-    [SerializeField] float rcsTrust = 100f;
-    [SerializeField] float mainTrust = 100f;
 
-    // Use this for initialization
-    void Start ()
+    void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-        Rotate();
-        Trusting();
     }
 
-    private void Trusting()
+    // Update is called once per frame
+    void Update()
+    {
+        if (state == State.Alive) {
+            RespondToThrustInput();
+            RespondToRotate();
+        }
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (state != State.Alive) { return; }
+
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+
+                break;
+            case "Finish":
+                StartSuccessSequence();
+                break;
+            default:
+                StartDeathSequence();
+                break;
+        }
+    }
+
+    private void StartSuccessSequence()
+    {
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(nextLevel);
+        nextLevelParticles.Play();
+        Invoke("LoadNextLevel", levelTimeDelay);
+    }
+
+    private void StartDeathSequence()
+    {
+        state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(death);
+        deathParticles.Play();
+        Invoke("LoadLevelOne", levelTimeDelay);
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1); //todo allow for more than 2 levels
+    }
+
+    private void LoadLevelOne()
+    {
+
+        SceneManager.LoadScene(0);
+    }
+
+    private void RespondToThrustInput()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up * mainTrust);
-            mySound = true;
+            ApplyThrust();
         }
         else
         {
-            mySound = false;
-        }
-        if (mySound == true && myToggle == false)
-        {
-            audioSource.Play();
-            myToggle = true;
-        }
-        if (mySound == false && myToggle == true)
-        {
             audioSource.Stop();
-            myToggle = false;
+            mainEngineParticles.Stop();
         }
     }
 
+    private void ApplyThrust()
+    {
+        rigidBody.AddRelativeForce(Vector3.up * mainTrust * Time.deltaTime);
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainEngine);
+        }
+        mainEngineParticles.Play();
+    }
 
-    private void Rotate()
+    private void RespondToRotate()
     {
         rigidBody.freezeRotation = true;  // take manual control of rotation
 
-        
+
         float rotationThisFrame = rcsTrust * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.A))
@@ -67,21 +124,4 @@ public class Rocket : MonoBehaviour {
         }
         rigidBody.freezeRotation = false; // resume physics control
     }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        switch (collision.gameObject.tag)
-        {
-            case "Friendly":
-                print("OK");
-                break;
-            case "Fuel":
-                print("Fuel");
-                break;
-            default:
-                print("Dead");
-                break;
-        }
-    }
-
 }
